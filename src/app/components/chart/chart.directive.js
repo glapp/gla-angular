dde53ab7aef4889c9cb4c7912b3951721dc0854d
Dinesh <hyperties@gmail.com>
2016-03-09 14:51:35 +0100

    Charts updated for cpu, response time  - local, zoomed versions - minor amend in details to remove the grid lines - data distributions for demo - minor cosmetic changes

--- src/app/components/chart/chart.directive.js
+++ src/app/components/chart/chart.directive.js
@@ -22,97 +22,24 @@
     return directive;
 
     /** @ngInject */
-    function DataController() {
+    function DataController($interval) {
       var vm = this;
-      vm.test = "test";
-      //barChart();
-      monitorChart();
-      //apiTest();
+      vm.toggle = "toggle";
+      vm.cpu = true;
+      cpuChart();
+      responseChart();
 
 
-      function barChart() {
-        vm.options = {
-          chart: {
-            type: 'discreteBarChart',
-            height: 450,
-            margin: {
-              top: 20,
-              right: 20,
-              bottom: 50,
-              left: 55
-            },
-            x: function (d) {
-              return d.label;
-            },
-            y: function (d) {
-              return d.value + (1e-10);
-            },
-            showValues: true,
-            valueFormat: function (d) {
-              return d3.format(',.4f')(d);
-            },
-            duration: 500,
-            xAxis: {
-              axisLabel: 'X Axis'
-            },
-            yAxis: {
-              axisLabel: 'Y Axis',
-              axisLabelDistance: -10
-            }
-          }
-        };
-
-        vm.data = [
-          {
-            key: "Cumulative Return",
-            values: [
-              {
-                "label": "A",
-                "value": -29.765957771107
-              },
-              {
-                "label": "B",
-                "value": 0
-              },
-              {
-                "label": "C",
-                "value": 32.807804682612
-              },
-              {
-                "label": "D",
-                "value": 196.45946739256
-              },
-              {
-                "label": "E",
-                "value": 0.19434030906893
-              },
-              {
-                "label": "F",
-                "value": -98.079782601442
-              },
-              {
-                "label": "G",
-                "value": -13.925743130903
-              },
-              {
-                "label": "H",
-                "value": -5.1387322875705
-              }
-            ]
-          }
-        ]
-      }
-
-      function monitorChart() {
+      function cpuChart() {
         vm.options = {
           chart: {
             type: 'lineChart',
             height: 180,
             margin: {
               top: 20,
-              right: 20,
+              right: 10,
               bottom: 40,
-              left: 55
+              left: 45
             },
             x: function (d) {
               return d.x;
@@ -122,31 +49,107 @@
             },
             useInteractiveGuideline: true,
             duration: 500,
+            xAxis: {
+              axisLabel: 'last 30 seconds'
+              //axisLabelDistance: 10
+            },
+            showXAxis: false,
             yAxis: {
               tickFormat: function (d) {
                 return d3.format('.01f')(d);
-              }
+              },
+              axisLabel: 'usage %',
+              axisLabelDistance: -15
             }
           }
         };
 
         vm.options1 = angular.copy(vm.options);
         vm.options1.chart.duration = 0;
-        vm.options1.chart.yDomain = [-1, 1];
+        vm.options1.chart.yDomain = [0, 100];
 
-        vm.data = [{values: [], key: 'CPU'}];
+        vm.data = [{values: [], key: 'CPU', color: "ed5818"}];
 
         vm.run = true;
 
         var x = 0;
-        setInterval(function () {
+        var min = 1;
+        var max = 10;
+        $interval(function () {
           if (!vm.run) return;
-          vm.data[0].values.push({x: x, y: Math.random() - 0.5});
-          if (vm.data[0].values.length > 20) vm.data[0].values.shift();
+          vm.data[0].values.push({x: x, y: Math.floor(Math.random() *(max - min) + min)});
+          if (vm.data[0].values.length > 30) vm.data[0].values.shift();
           x++;
 
           vm.$apply(); // update both chart
         }, 2500);
+        $interval(function(){
+          if(max <100){
+            max += 5;
+            min +=3
+          }
+        }, 9000);
+      }
+
+      function responseChart() {
+        vm.options3 = {
+          chart: {
+            type: 'lineChart',
+            height: 180,
+            margin: {
+              top: 20,
+              right: 10,
+              bottom: 40,
+              left: 45
+            },
+            x: function (d) {
+              return d.x;
+            },
+            y: function (d) {
+              return d.y;
+            },
+            useInteractiveGuideline: true,
+            duration: 500,
+            xAxis: {
+              axisLabel: 'last 30 seconds'
+              //axisLabelDistance: 10
+            },
+            showXAxis: false,
+            yAxis: {
+              tickFormat: function (d) {
+                return d3.format('.01f')(d);
+              },
+              axisLabel: 'response time',
+              axisLabelDistance: -15
+            }
+          }
+        };
+
+        vm.options4 = angular.copy(vm.options3);
+        vm.options4.chart.duration = 0;
+        vm.options4.chart.yDomain = [10, 500];
+
+        vm.data3 = [{values: [], key: 'ms'}];
+
+        vm.run = true;
+
+        var x = 0;
+        var min = 10;
+        var max = 50;
+        $interval(function () {
+          if (!vm.run) return;
+          vm.data3[0].values.push({x: x, y: Math.floor(Math.random() *(max - min) + min)});
+          if (vm.data3[0].values.length > 30) vm.data3[0].values.shift();
+          x++;
+
+          vm.$apply(); // update both chart
+        }, 2000);
+        $interval(function(){
+          if(max <480){
+            max += 15;
+            min +=8
+          }
+        }, 6000);
       }
 
     }
