590efec29c2b20c96d32f0ab21f0ac7b9799c466
Fabio <islerfab@gmail.com>
2016-05-13 16:33:46 +0300

    added scaleUp button

---
+++ src/app/components/Organ/Organ.service.js
@@ -0,0 +1,17 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('glaAngular')
+    .service('Organ', Organ);
+
+    function Organ($resource) {
+      return $resource('http://localhost:1337/organ/:id', {
+          id: '@_id'
+        },
+        {
+          scaleUp: {method: 'POST', url: 'http://localhost:1337/organ/scaleUp', isArray: false}
+        }
+      );
+    }
+})();
