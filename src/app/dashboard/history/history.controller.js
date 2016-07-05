749ce29b1065e59308afff025d94a96539556dba
Dinesh <hyperties@gmail.com>
2016-07-05 17:12:40 +0200

    New history page - displays application log, adaptions

--- src/app/dashboard/history/history.controller.js
+++ src/app/dashboard/history/history.controller.js
@@ -1,3 +1,38 @@
-/**
- * Created by maverick on 24/5/2016.
- */
+(function() {
+  'use strict';
+
+  angular
+    .module('glaAngular')
+    .controller('HistoryController', HistoryController);
+
+  /** @ngInject */
+  function HistoryController($stateParams, Policy, Application, toastr, $log) {
+
+    var vm = this;
+
+    vm.logs = getLog();
+
+
+// To get the change logs associated with the given application ID
+    function getLog(){
+      var items = [];
+      Application.getAppDetails({app_id: $stateParams.app_id},
+        function onSuccess(response) {
+          response.log.forEach(function(log){
+            items.push(log);
+          });
+
+        }, function onError(err) {
+          toastr.error(err.data, 'Error');
+        });
+
+      $log.info(items);
+      return items;
+    }
+
+  }
+})();
+
+
+
+
