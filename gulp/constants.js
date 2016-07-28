99c1669833a8d6425f05ea09ad3639adca75febe
Fabio <islerfab@gmail.com>
2016-07-28 23:36:53 +0200

    New try of resolving host of sails for angular - still doesn't resolve

--- gulp/constants.js
+++ gulp/constants.js
@@ -9,7 +9,7 @@ gulp.task('constants', function () {
   return gulp.src('gulp/sails-host.json')
     .pipe(ngConstant({
       name: 'glaAngular.config',
-      constants: {SAILS_HOST: process.env.SAILS_HOST || 'localhost:1337' }
+      constants: {SAILS_HOST: (process.env.SAILS_HOST || 'gla_sails') + ':' + (process.env.SAILS_PORT || '1337')}
     }))
     .pipe(gulp.dest(path.join(conf.paths.tmp, '/serve/app')));
 });
